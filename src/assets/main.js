8efff8e534f6f68b19c7d318c1e113eb585dcf04
Dark4Angel <quakextreme@msn.com>
2024-03-10 02:03:39 -0500

    lastupdate     lastupdate

--- src/assets/main.js
+++ src/assets/main.js
@@ -3,13 +3,15 @@ const content=null||document.getElementById('content');
 const options={method:'GET',headers:{
     'X-RapidAPI-Host':'youtube-v31.p.rapidapi.com',
     'X-RapidAPI-Key':'c68edbdba9mshc3cb6eb8038f3b2p1f444ajsna5de19fe1256'}};
+    
     async function fetchData(urlApi){
         const response=await fetch(urlApi,options);
         const data=await response.json();
         return data;}
 
-(async()=>{try{const videos=await fetchData(API);let view=`
-    ${videos.items.map(video=>`
+(async()=>{
+    try{const videos=await fetchData(API);
+        let view=`${videos.items.map(video=>`
       <div class="group relative">
         <div
           class="w-full bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:aspect-none">
